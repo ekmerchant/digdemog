86de6aaba3b9b6c14029761a053e96d2f687b1af
Emily Klancher Merchant <ekmerchant@ucdavis.edu>
2021-04-02 20:59:18 -0700

    updated title.js with new url

--- title.js
+++ title.js
@@ -2,7 +2,7 @@
 
 document.getElementById("name")
 .innerHTML =
-"<a class='name' href = 'http://www.emilyklancher.com/digdemog/index.html'>A Digital Reading \
+"<a class='name' href = 'https://ekmerchant.github.io/digdemog/'>A Digital Reading \
 of<br>&nbsp;&nbsp;Twentieth Century Demography</a><br><a class = 'email' href='http://www.emilyklancher.com'>Emily Klancher Merchant</a><hr/>"
     
 -->
